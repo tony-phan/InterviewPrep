75b1e502328ac9cfaf7cb13be48a435e60131e22
Anthony Tran-Phan <tonyphan15@gmail.com>
2021-09-24 20:07:14 -0700

    Small edit

--- Heaps/Find K Pairs with Smallest Sums/Find-K-Pairs-with-Smallest-Sums(Optimized).java
+++ Heaps/Find K Pairs with Smallest Sums/Find-K-Pairs-with-Smallest-Sums(Optimized).java
@@ -1,3 +1,4 @@
+}
 class Solution {
     /*
     Match
@@ -7,6 +8,10 @@ class Solution {
     loop through nums1 and num2 to create pairings
     Add the pairings into the minHeap, remove elements as needed if the heap size > k
     put all elements in heap into result array and return
+                              0 1 2
+    nums1 = [1,1,2], nums2 = [1,2,3], k = 3
+    heap: [[1,1,0],[1,1,0],[2,1,0]]
+    result: []
     
     Evaluate
     Time: O(n)
@@ -15,41 +20,29 @@ class Solution {
     */
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         List<List<Integer>> result = new ArrayList<>();
-        
-        Comparator<List<Integer>> comparator = new Comparator<>() {
+        Queue<List<Integer>> heap = new PriorityQueue<>(new Comparator<List<Integer>>() {
             @Override
             public int compare(List<Integer> pair1, List<Integer> pair2) {
-                int pair1Sum = pair1.get(0) + pair1.get(1), pair2Sum = pair2.get(0) + pair2.get(1);
+                int pair1Sum = pair1.get(0) + pair1.get(1);
+                int pair2Sum = pair2.get(0) + pair2.get(1);
                 
-                if(pair1Sum == pair2Sum) {
-                    return 0;
-                }
-                else if(pair1Sum < pair2Sum) {
-                    return -1;
-                }
-                else {
-                    return 1;
-                }
+                return pair1Sum - pair2Sum;
             }
-        };
-        
-        Queue<List<Integer>> heap = new PriorityQueue<>(comparator);
+        });
         
         for(int i = 0; i < nums1.length; ++i) {
             heap.add(Arrays.asList(nums1[i], nums2[0], 0));
         }
         
-        while(k > 0 && !heap.isEmpty()) {
-            --k;
+        while(result.size() != k && !heap.isEmpty()) {
             List<Integer> pair = heap.poll();
             result.add(Arrays.asList(pair.get(0), pair.get(1)));
             
-            if(nums2.length - 1 == pair.get(2)) {
-                continue;
+            if(pair.get(2) == nums2.length - 1) {
+               continue; 
             }
-            
             heap.add(Arrays.asList(pair.get(0), nums2[pair.get(2) + 1], pair.get(2) + 1));
-        }        
+        }
         return result;
     }
 }

c7867782177bab95d736c034cb86adcdaabd4389
Anthony Tran-Phan <tonyphan15@gmail.com>
2025-08-26 21:27:01 -0700

    House Robber (Leetcode #198)

--- Dynamic Programming/House-Robber.cs
+++ Dynamic Programming/House-Robber.cs
@@ -1,22 +1,18 @@
-public class Solution
-{
-    public int Rob(int[] nums)
-    {
+public class Solution {
+    public int Rob(int[] nums) {
         int n = nums.Length;
+        if(n == 1) return nums[0];
+        if(n == 2) return Math.Max(nums[0], nums[1]);
+
         int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.Max(nums[0], nums[1]);
 
-        for (int i = 0; i < n; ++i)
-        {
-            if (i < 2)
-            {
-                dp[i] = nums[i];
-                continue;
-            }
-
-            int maxAmountRobbed = Math.Max(nums[i] + dp[i - 2], dp[i - 1]);
+        for(int i = 2; i < n; ++i) {
+            int maxAmountRobbed = Math.Max(nums[i] + dp[i-2], dp[i-1]);
             dp[i] = maxAmountRobbed;
         }
-        return dp[n - 1];
+        return dp[n-1];
     }
 }
 
